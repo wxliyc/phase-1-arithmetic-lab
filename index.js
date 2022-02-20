270b4c7870eba9afaa9ae50289ac2419692fe333
Ethan Lee <wxliyc@gmail.com>
2022-02-20 15:33:23 -0600

    commit tree 53c5701b4ea95609fe79852e6c4f04ca8003209e

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 1
+let num2 = 62
+let multiply=num1 * num2 
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) +1;
+
+let num3 = 68
+let num4 = 8
+let mod=num3 % num4
+
+let max = Math.max(3, 5, 20)
